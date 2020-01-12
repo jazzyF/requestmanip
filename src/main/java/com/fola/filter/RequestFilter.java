985a6b3ece6bd92d97ecafcf4cb4ae59fd3dc98f
Fola Ayinde <folabi.ayinde@gmail.com>
2020-01-12 17:57:05 -0500

    Changed filter implementation to OnceRerRequestFilter

--- src/main/java/com/fola/filter/RequestFilter.java
+++ src/main/java/com/fola/filter/RequestFilter.java
@@ -3,21 +3,22 @@ package com.fola.filter;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class RequestFilter implements Filter {
+public class RequestFilter extends OncePerRequestFilter {
+
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        servletRequest.setAttribute("filterUUID", UUID.randomUUID());
-        filterChain.doFilter(servletRequest, servletResponse);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        request.setAttribute("filterUUID", UUID.randomUUID());
+        filterChain.doFilter(request, response);
     }
 }
